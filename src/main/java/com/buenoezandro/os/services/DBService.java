7d4e12c4f37a18bcec67274cee01ece88d076b25
ezbueno <ezandrobueno@yahoo.com.br>
2021-06-27 13:18:35 -0300

    Adicionado mais dados para inserção no BD

--- src/main/java/com/buenoezandro/os/services/DBService.java
+++ src/main/java/com/buenoezandro/os/services/DBService.java
@@ -32,16 +32,29 @@ public class DBService {
 
 	public void inserirDadosTeste() {
 		var t1 = new Tecnico(null, "João", "311.239.896-30", "(11) 99999-9999");
-		var t2 = new Tecnico(null, "Pedro", "200.333.950-71", "(11) 98888-8888");
 		var c1 = new Cliente(null, "Carlos", "996.503.842-27", "(11) 98999-8888");
 		var os1 = new OrdemServico(null, Prioridade.ALTA, "Teste de criação de OS", Status.ANDAMENTO, t1, c1);
 
 		t1.getList().add(os1);
 		c1.getList().add(os1);
-
-		this.tecnicoRepository.saveAll(Arrays.asList(t1, t2));
-		this.clienteRepository.saveAll(Arrays.asList(c1));
-		this.ordemServicoRepository.saveAll(Arrays.asList(os1));
+		
+		var t2 = new Tecnico(null, "Pedro", "200.333.950-71", "(11) 98888-8888");
+		var c2 = new Cliente(null, "Paulo", "214.288.100-99", "(11) 98777-6666");
+		var os2 = new OrdemServico(null, Prioridade.MEDIA, "Teste de criação de OS 2", Status.ANDAMENTO, t2, c2);
+		
+		t2.getList().add(os2);
+		c2.getList().add(os2);
+		
+		var t3 = new Tecnico(null, "Marcos", "267.500.910-59", "(11) 95555-2222");
+		var c3 = new Cliente(null, "Felipe", "243.507.240-60", "(11) 92222-4444");
+		var os3 = new OrdemServico(null, Prioridade.BAIXA, "Teste de criação de OS 3", Status.ANDAMENTO, t3, c3);
+		
+		t3.getList().add(os3);
+		c3.getList().add(os3);		
+		
+		this.tecnicoRepository.saveAll(Arrays.asList(t1, t2, t3));
+		this.clienteRepository.saveAll(Arrays.asList(c1, c2, c3));
+		this.ordemServicoRepository.saveAll(Arrays.asList(os1, os2, os3));
 	}
 
 }
